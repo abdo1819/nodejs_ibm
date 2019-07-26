b56b846e83841511e348061f44f69b9a866896a1
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-07-26 16:51:58 +0200

    fetch with metods new notation

--- hellofetch/home.js
+++ hellofetch/home.js
@@ -3,7 +3,7 @@ var fetch = require("node-fetch");
 var port = 8080;
 http.createServer(function (request, response) {
 	fetch('http://localhost:8081/?city=Cairo', {method: 'get'})
-	.then (function(res) {return res.text()})
-	.then(function (data) {response.end('Hello! ' + data)})
-	.catch(function(e) {console.log(e.message)});		
+	.then (res =>  res.text())
+	.then(data => response.end('Hello! ' + data))
+	.catch(e => {console.log(e.message)});		
 }).listen(port);
