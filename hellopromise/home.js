7726277bef7e7dc261d09ae879f0be744795d216
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-07-26 16:46:59 +0200

    make generic promise

--- hellopromise/home.js
+++ hellopromise/home.js
@@ -1,28 +1,30 @@
 var http = require("http");
 var port = 8080;
-function readWeather(options, response) {
-    var weatherRequest = http.request(options, function (weatherResponse) {
-        var buffer = '';
-        var result = '';
-        weatherResponse.on('data', function (chunk) {
-            buffer += chunk;
+function readWeather(options) {
+    return new Promise(function (resolve, reject) {
+        var weatherRequest = http.request(options, function (weatherResponse) {
+            var buffer = '';
+            var result = '';
+            weatherResponse.on('data', function (chunk) {
+                buffer += chunk;
+            });
+            weatherResponse.on('end', function () {
+                resolve('Hello! ' + buffer);
+
+            });
         });
-        weatherResponse.on('end', function () {
-            response.end('Hello! ' + buffer);
+        weatherRequest.on('error', function (e) {
+            reject(e.message);
         });
-    });
-    weatherRequest.on('error', function (e) {
-        console.log(e.message);
-    });
-    weatherRequest.end();
+        weatherRequest.end();
+    })
 }
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     var cityName = 'Cairo';
     var options = {
-        hotst: 'localhost',
         path: '/?city=' + cityName,
         port: '8081'
     };
-    readWeather(options, response);
+    readWeather(options).then(val => response.end(val), console.log);
 }).listen(port);
